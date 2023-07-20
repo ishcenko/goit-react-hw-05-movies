85085c689fe678b4cb1dd79eb1ecba974aa8a21c
ishcenko <107360109+ishcenko@users.noreply.github.com>
2023-07-20 21:44:53 +0300

    reset the input field after submitting the search

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -3,17 +3,20 @@ import PropTypes from 'prop-types';
 import { Form, Input, Button } from './SearchForm.styled';
 
 function SearchForm({ onSubmit, defaultValue }) {
-  const searhRef = useRef();
+  const searchRef = useRef();
 
   const handleSubmitForm = event => {
     event.preventDefault();
 
-    onSubmit(searhRef.current.value);
+    const searchTerm = searchRef.current.value;
+    onSubmit(searchTerm);
+
+    searchRef.current.value = '';
   };
 
   return (
     <Form onSubmit={handleSubmitForm}>
-      <Input defaultValue={defaultValue} ref={searhRef} type="text" required />
+      <Input defaultValue={defaultValue} ref={searchRef} type="text" required />
       <Button>Search</Button>
     </Form>
   );
